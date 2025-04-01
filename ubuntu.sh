705ef24bad55ff040705bfce0dfdce593f6e0409
Kentaro Hayashi <hayashi@clear-code.com>
2025-04-01 11:41:19 +0900

    Show ubuntu:noble

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,8 @@
 
 set -ux
 
+# See https://images.linuxcontainers.org/
+
 echo "::group::Run test: show info ubuntu:n"
 sudo incus image info ubuntu:n
 echo "::endgroup::"
@@ -18,10 +20,12 @@ echo "::group::Run test: show info noble"
 sudo incus image info noble
 echo "::endgroup::"
 
+echo "::group::Run test: show info ubuntu:noble"
+sudo incus image list ubuntu:noble
+echo "::endgroup::"
+
 echo "::group::Run test: list ubuntu images"
 sudo incus image list ubuntu:
 echo "::endgroup::"
-
-
 
 echo -e "\nAll check has passed!\n"
