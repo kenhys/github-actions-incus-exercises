8e23294b16ac37c85a45290231e0b95494d4e200
Kentaro Hayashi <hayashi@clear-code.com>
2026-02-14 21:24:11 +0900

    Show .repo snapshot

--- info.sh
+++ info.sh
@@ -6,6 +6,7 @@ if [ -x /usr/bin/dnf ]; then
     dnf install -y openssl-devel
     dnf update -y
     rpm -qa | grep openssl
+    cat /etc/yum.repos.d/*.repo
 elif [ -x /usr/bin/apt ]; then
     apt show libssl3t64
 else
